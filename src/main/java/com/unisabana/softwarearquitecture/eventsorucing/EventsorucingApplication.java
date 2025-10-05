186decb0fcd8b77fd40fa5011dc35539356a3317
Sebastián Joya <joyajuan@unbosque.edu.co>
2025-10-04 19:59:46 -0500

    Initial Commit

---
+++ src/main/java/com/unisabana/softwarearquitecture/eventsorucing/EventsorucingApplication.java
@@ -0,0 +1,13 @@
+package com.unisabana.softwarearquitecture.eventsorucing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsorucingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsorucingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unisabana/softwarearquitecture/eventsorucing/EventsorucingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisabana.softwarearquitecture.eventsorucing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsorucingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
